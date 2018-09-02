f5fa7f31863c067876822f439ded4cd7ba8e1d9d
Eric Rangell <ericrangell@Erics-Air.home>
2018-09-02 04:04:18 -0400

    restructured to save memory

--- ChipTester.ino
+++ ChipTester.ino
@@ -1,21 +1,24 @@
 // Reactive Micro Chip Tester
 // (c) 2018, Reactive Micro.
 
-int verboseDebug = 0; //set to 1 for more log messages
-
 #include <SD.h>
-File root;
-int SDconnected = 0; //will get set to 1 if successful connection
-int testCount = 0;
-int testFileCount = 0;
-const int MAXTESTS = 5;
-const int MAXTFILES = 5;
-String testDirs[MAXTESTS];
-String testFiles[MAXTFILES];
-
 
 //Set to pin number used by your SD shield. (Data logger shield=10)
 int SDPIN = 10;
+
+int SDconnected = 0; //will get set to 1 if successful connection
+int testCount = 0;
+int testFileCount = 0;
+String testDirName = "";
+char testCmd = ' ';
+String testParm = "";
+int testState = 0;
+int testParmCtr = 0;
+String testFileName = "";
+//int pinMap[40];
+//String pinName[40];
+//int pinIO[40];
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -35,41 +38,34 @@ void setup() {
   SDconnected = 1;
   Serial.println("initialization done.");
 
-  root = SD.open("/");
-  printDirectory(root, 0);
-  root.close();
-    
-}
+  printDirectory();
 
-
-void loop() {
-  if (SDconnected == 0)
-    return;
-
-  Serial.println ("Enter number of directory containing tests to run:");  
+  Serial.println ("Enter name of directory containing tests to run:");  
 
   while (!Serial.available())
   {
     delay(100);
   }
 
-  String testDirNum = Serial.readStringUntil('\n');
-  int iDirNum = testDirNum.toInt();
-  Serial.println(iDirNum);
-  if ((iDirNum <= 0) || (iDirNum > testCount)) 
-  {
-    Serial.println("Invalid option");
-    testDirNum = Serial.readStringUntil('\n');
+  testDirName = Serial.readStringUntil('\n');
+
+  ReadTestDir(testDirName);
+}
+
+
+void loop() {
+
+  if (SDconnected == 0)
     return;
-  }
-  
-  Serial.print("Selected test set: ");
-  Serial.println(testDirNum);
-  ReadTests(iDirNum-1);
-  ExecTests(iDirNum-1);
+    
+    Serial.println("Enter name of test file to run:");
+    while (!Serial.available())
+    {
+      delay(100);
+    }
 
-
-  printTestDirectoryArray();
+    testFileName = Serial.readStringUntil('\n');
+    ExecuteTestFile( testFileName, testDirName );
 }
 
 void establishContact() {
@@ -82,68 +78,45 @@ void establishContact() {
   Serial.println();
 }
 
-void printDirectory(File dir, int numTabs) {
+void printDirectory() {
+  File root = SD.open("/");
+
   Serial.println(root.name());
   Serial.println("Test Directories in root of SD card:");
   testCount = 0;
   while (true) {
 
-    File entry =  dir.openNextFile();
+    File entry =  root.openNextFile();
     if (! entry) {
       // no more files
       break;
     }
-    //for (uint8_t i = 0; i < numTabs; i++) {
-    //  Serial.print('\t');
-    //}
+
     if (entry.isDirectory()) {
       if (entry.name()[0]=='T')
       {
-        if (testCount < MAXTESTS)
-        {
           testCount++;
-          Serial.print (testCount);
-          Serial.print (" : ");
           Serial.println(entry.name());
-          testDirs[testCount - 1] = entry.name();          
-        }
       }
     }
     entry.close();
   }
-}
-
-void printTestDirectoryArray()
-{
-  for (int i=0; i < testCount; i++)
-  {
-    Serial.print (i+1);
-    Serial.print (" : ");
-    Serial.println(testDirs[i]);
-  }
+  root.close();
 }
 
 
-void printTestFileArray()
+void ReadTestDir(String testDirName)
 {
-  for (int i=0; i < testFileCount; i++)
-  {
-    Serial.print (i+1);
-    Serial.print (" : ");
-    Serial.println(testFiles[i]);
-  }
-}
-
-void ReadTests(int testDirNum)
-{
-  File selectedTestDir;
   testFileCount = 0;
-  String dir2open = testDirs[testDirNum];
-  Serial.println("Opening: "+dir2open);
-
-  selectedTestDir = SD.open(dir2open);
+  File selectedTestDir = SD.open(testDirName);
+  if (!selectedTestDir)
+  {
+    Serial.println("Directory not found");
+    return;
+  }
+  File testSet;
   while (true) {
-    File testSet = selectedTestDir.openNextFile();
+    testSet = selectedTestDir.openNextFile();
     if (!testSet) {
       break;
     }
@@ -151,81 +124,23 @@ void ReadTests(int testDirNum)
     String testSetFile = testSet.name();
     if (testSetFile[0] == 'T')
     {
-      if (verboseDebug) Serial.println("Test File="+testSetFile);
-    
-      testFiles[testFileCount] = testSetFile;  
-      testSet.close();
+      Serial.println(testSetFile);
       testFileCount++;
-    
-      //INSERTION SORT - determine if this file needs to bubble up
-      int currTestIndex = testFileCount - 1;
-      String currTest = testFiles[currTestIndex];
-      String currTestNum = currTest.substring(1,4);
-      if (verboseDebug) Serial.println("currTestNum="+currTestNum);
-
-      if (testFileCount > 1)
-      {
-        int newFileNum = currTestNum.toInt();
-        if (verboseDebug) {
-          Serial.print ("newFileNum=");
-          Serial.println(newFileNum);
-        }
-        
-        int prevFileIndex = testFileCount - 2;
-        while (prevFileIndex >= 0)
-        {
-          String prevFileNumStr = testFiles[prevFileIndex].substring(1,4);
-          int prevFileNum = prevFileNumStr.toInt();
-          if (newFileNum < prevFileNum)
-          {
-            if (verboseDebug) Serial.println("***SWAPPING***");
-
-            testFiles[currTestIndex] = testFiles[prevFileIndex];
-            testFiles[prevFileIndex] = currTest;
-            currTestIndex = prevFileIndex;
-            currTest = testFiles[currTestIndex];
-            currTestNum = currTest.substring(1,4);
-            newFileNum = currTestNum.toInt();
-          }
-          
-          prevFileIndex--;
-        }
-        if (verboseDebug) printTestFileArray();
-
-      }
-    }
+    } 
   }
+  testSet.close();
+  
   selectedTestDir.close();
   if (testFileCount == 0)
   {
     Serial.println("No test files found");
   }
-  else
-  {
-    Serial.println("TEST FILES TO BE RUN:");
-    printTestFileArray();
-  }
-  Serial.println();
 }
 
-
-void ExecTests(int testDirIndex)
-{
-  for (int i=0; i < testFileCount; i++)
-  {
-    Serial.print ("Enter any text and press <CR> to execute TEST file: ");
-    Serial.print (i+1);
-    Serial.print (" : ");
-    Serial.println(testFiles[i]);
-    establishContact();
-    ExecTestFile(testFiles[i], testDirIndex);
-  }
-}
-
-void ExecTestFile(String testFile, int testDirIndex)
+void ExecuteTestFile(String testFile, String testFileDir)
 {
   String csvline;
-  String testFilePath = "/" + testDirs[testDirIndex] + "/" + testFile;
+  String testFilePath = "/" + testFileDir + "/" + testFile;
   Serial.println("Opening: "+testFilePath);
 
   File csvFile = SD.open(testFilePath);
@@ -234,11 +149,57 @@ void ExecTestFile(String testFile, int testDirIndex)
     while (csvFile.available()) {
       csvline = csvFile.readStringUntil('\n');
       Serial.println(csvline);
+      
+      char command = csvline[0];
+      //Serial.println(command);
+      TestCommand(command);
+      
+      int commapos = csvline.indexOf(',',0);
+      int lastcomma = csvline.lastIndexOf(',');
+      while (commapos < lastcomma)
+      {
+        int nextcomma = csvline.indexOf(',',commapos+1);
+        String param = csvline.substring(commapos+1,nextcomma);      
+        //Serial.println(param); 
+        TestParam(param);
+        commapos = nextcomma;
+      }
+      String lastparam = csvline.substring(lastcomma+1,csvline.length());
+      //Serial.println(lastparam);
+      TestParam(lastparam);
     }
-   csvFile.close();
   }
   else {
     Serial.println("ERROR opening "+testFilePath);
   }
+  csvFile.close();
+}
+
+void TestCommand(char c)
+{
+  testCmd = c;
+  testParmCtr = 0;
+}
+
+void TestParam(String p)
+{
+  testParm = p;
+  switch (testCmd) {
+    case 'M': {
+      break;    
+    }
+    case 'N': {
+      break;
+    }
+      
+    case 'O': {
+      break;
+    }
+      
+    case 'P': {
+     break; 
+    }    
+  }
+   
 }
 
